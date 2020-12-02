6be172861628577a0a4e30f9885c12e77811949b
wkn816 <wkn816@gmail.com>
2020-12-02 22:10:49 +0900

    検索フォームのviews追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,12 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+
+  def User.search(search, user_or_post)
+    if user_or_post == "1"
+       User.where(['name LIKE ?', "%#{search}%"])
+    else
+       User.all
+    end
+end
 end
